6c2e7ddd1f5f0b4d948ac699519e0fd3fce5b083
Leandro Santos <lds.leosantos@gmail.com>
2023-06-16 08:07:25 -0300

    06 - Autenticar API 05 - Recuperar Usuário Autenticado API Laravel

--- app/Repositories/Traits/RepositoryTrait.php
+++ app/Repositories/Traits/RepositoryTrait.php
@@ -4,11 +4,10 @@ namespace app\Repositories\Traits;
 
 use App\Models\User;
 
-
 trait RepositoryTrait
 {
     private function getUserAuth(): User
     {
-        return User::first();
+        return auth()->user();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,22 +7,30 @@ use App\Http\Controllers\Api\{
     ModuleController,
     LessonController,
     ReplySupportController,
-    SupportController
+    SupportController,
+
 };
+use App\Http\Controllers\Api\Auth\AuthController;
+
+Route::post('/auth', [AuthController::class,'auth']);
+Route::post('/logout', [AuthController::class,'logout'])->middleware('auth:sanctum');
+Route::get('/me', [AuthController::class,'me'])->middleware('auth:sanctum');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::get('/courses', [CourseController::class,'index']);
+    Route::get('/courses/{id}', [CourseController::class,'show']);
+    Route::get('/courses/{id}/modules', [ModuleController::class,'index']);
+    Route::get('/modules/{id}/lessons', [LessonController::class,'index']);
+    Route::get('/lessons/{id}', [LessonController::class,'show']);
+    Route::get('/supports', [SupportController::class,'index']);
+    Route::get('/my-supports', [SupportController::class,'mySupports']);
+    Route::post('/supports', [SupportController::class,'store']);
+    Route::post('/replies', [ReplySupportController::class,'createReply']);
+
+});
 
 
-
-Route::get('/courses', [CourseController::class,'index']);
-Route::get('/courses/{id}', [CourseController::class,'show']);
-Route::get('/courses/{id}/modules', [ModuleController::class,'index']);
-Route::get('/modules/{id}/lessons', [LessonController::class,'index']);
-Route::get('/lessons/{id}', [LessonController::class,'show']);
-Route::get('/supports', [SupportController::class,'index']);
-Route::get('/my-supports', [SupportController::class,'mySupports']);
-
-Route::post('/supports', [SupportController::class,'store']);
-
-Route::post('/replies', [ReplySupportController::class,'createReply']);
 
 Route::get('/', function(){
     return response()->json([
